59e41108d29feb95e2fb04d592c23f06c5f6c01b
pdlma0904 <57673815+pdlma0904@users.noreply.github.com>
2020-12-30 12:20:27 +0900

    study useReducer

---
+++ useReducer/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <Info />
+    </>
+  );
+}
+
+export default App;
