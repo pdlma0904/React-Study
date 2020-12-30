77d197629788d89444cde0c22cd677742a626df4
pdlma0904 <57673815+pdlma0904@users.noreply.github.com>
2020-12-30 12:19:53 +0900

    study useState

---
+++ useState/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <br/>
+      <Info />
+    </>
+  );
+}
+
+export default App;
